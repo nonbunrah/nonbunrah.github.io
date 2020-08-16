5b8b96b18047f6b56f4ea8f1c3ad9a00d02316dd
nonbunrah <rj.bamrah@gmail.com>
2020-08-15 21:56:54 -0700

    making new repo for portfolio page

---
+++ src/Header/HeaderComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import github from '../img/github.png';
+import linked from '../img/linked.svg';
+import './Header.css';
+
+function HeaderComponent(props) {
+  const { gitLink, profileLink} = props;
+
+  return (
+    <div className='HeaderComponent'>
+      <header className='header'>
+        <div className='alignHeader'>
+          <h1 className='name'>RJ Bamrah</h1>
+          <img
+            alt='github-logo'
+            src={github}
+            onClick={gitLink}
+            className='github-logo'
+          ></img>
+          <img
+            alt='linkedin-logo'
+            src={linked}
+            onClick={profileLink}
+            className='linkedin-logo'
+          ></img>
+          <h1 className='about-me'>About Me</h1>
+          <h1 className='project'>Projects</h1>
+        </div>
+      </header>
+    </div>
+  )
+};
+
+export default HeaderComponent;
