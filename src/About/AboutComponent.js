af6b643c72478d977d1766f18d364bd523b5c676
nonbunrah <rj.bamrah@gmail.com>
2020-08-18 21:12:34 -0700

    added pictures to technologies section

--- src/About/AboutComponent.js
+++ src/About/AboutComponent.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './About.css';
+import css from '../img/css-logo.svg';
+import html from '../img/html-logo.svg';
+import js from '../img/js-logo.svg';
+import node from '../img/node-logo.svg';
+import logo from '../img/react-logo.svg';
+import sql from '../img/sql-logo.svg';
+import express from '../img/ExpressJS.png';
 
 function AboutComponent() {
   return (
@@ -8,21 +15,22 @@ function AboutComponent() {
         About Me
       </h1>
       <div className='row'>
-        <div className='column'>
+        <div className='about-column'>
         <p>
-          Hi! I am RJ Bamrah, and I am a Full Stack Software Engineer from the Bay Area. My passion for web 
-          development was sparked when I realized that I could create something by my own hand. There is 
-          nothing more I love than creating functional websites!
+          Hi! I am RJ Bamrah, and I am a<br /> Full Stack Software Engineer from the Bay Area.<br /> My passion for web 
+          development was sparked<br /> when I realized that I could create something<br /> by my own hand. There is 
+          nothing more I love<br /> than creating functional websites!
         </p>
         </div>
         <div className='column'>
-        <h2 className='tech'>Technologies</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-          esse cillum dolore eu fugiat nulla pariatur.
-        </p>
+          <h2 className='tech'>Technologies</h2>
+          <img className='logos' src={html} alt='html' />
+          <img className='logos' src={css} alt='css' />
+          <img className='logos' src={js} alt='javascript' />
+          <img className='logos' src={node} alt='node' />
+          <img className='logos' src={logo} alt='react' />
+          <img className='logos' src={sql} alt='sql' />
+          <img className='express-logo' src={express} alt='express' />
         </div>
       </div>
     </div>
