6921b4dae032eb0d7941872a2ec82ac461ce4300
nonbunrah <rj.bamrah@gmail.com>
2020-08-28 14:04:23 -0700

    put project in imac screen

--- src/Projects/ProjectComponent.js
+++ src/Projects/ProjectComponent.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Projects.css';
 import fourthProject from '../img/fourthProject.png';
+import imac from '../img/iMac.png';
 
 function ProjectComponent() {
   return (
     <div className='projectComponent'>
-      <img
-        src={fourthProject}
-        alt='fourth-project-pic'
-        className='fourth-project-pic'
-      ></img>
+      <div className='imac-with-project'>
+        <img
+          src={imac}
+          alt='imac'
+          className='imac'
+        ></img>
+        <img
+          src={fourthProject}
+          alt='fourth-project-pic'
+          className='fourth-project-pic'
+        ></img>
+      </div>
       {/* <div className='hidden-text'>
         <h1>Full Stack Calendar Scheduling App</h1>
       </div> */}
-      <div className='trans'></div>
+      {/* <div className='trans'></div> */}
     </div>
   )
 }
