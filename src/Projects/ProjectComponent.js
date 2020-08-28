289e526217df3f4509f474431b4b7037ee0cb586
nonbunrah <rj.bamrah@gmail.com>
2020-08-28 12:30:31 -0700

    added first project pic

--- src/Projects/ProjectComponent.js
+++ src/Projects/ProjectComponent.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Projects.css';
+import fourthProject from '../img/fourthProject.png';
 
 function ProjectComponent() {
   return (
-    <div>
-      <h1>ProjectComponent</h1>
+    <div className='projectComponent'>
+      <img
+        src={fourthProject}
+        alt='fourth-project-pic'
+        className='fourth-project-pic'
+      ></img>
+      <div className='hidden-text'>
+        <h1>Full Stack Calendar Scheduling App</h1>
+      </div>
     </div>
   )
 }
