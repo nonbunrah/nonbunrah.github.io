2d81aaf98c5b65a9a9bdcfbfbfce83ff0ec08b0c
nonbunrah <rj.bamrah@gmail.com>
2020-08-31 12:54:29 -0700

    added bcounter to projects

--- src/Projects/ProjectComponent.js
+++ src/Projects/ProjectComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import fourthProject from '../img/fourthProject.png';
+import bcounter from '../img/bcounter.png';
 import imac from '../img/iMac.png';
 
 function ProjectComponent() {
@@ -17,6 +18,20 @@ function ProjectComponent() {
           alt='fourth-project-pic'
           className='fourth-project-pic'
         ></img>
+      </div>
+      <div className='projectComponent2'>
+      <div className='imac-with-project'>
+        <img
+          src={imac}
+          alt='imac'
+          className='bcounter-imac'
+        ></img>
+        <img
+          src={bcounter}
+          alt='bcounter-pic'
+          className='bcounter'
+        ></img>
+      </div>
       </div>
       {/* <div className='hidden-text'>
         <h1>Full Stack Calendar Scheduling App</h1>
