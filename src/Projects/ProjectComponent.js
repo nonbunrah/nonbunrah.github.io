fd9e5b444511ff19413153a7417473e6fb0ad7f6
nonbunrah <rj.bamrah@gmail.com>
2020-09-09 14:26:40 -0700

    changed tech stack for each project

--- src/Projects/ProjectComponent.js
+++ src/Projects/ProjectComponent.js
@@ -99,7 +99,7 @@ function ProjectComponent() {
             >
           <br /><br /><br /><br /><br /><br /><br /><br />
             <h1 className='title-application'>Car Quiz Front End Project</h1>
-            <p className='title-tech'>Technologies used: React.js, Node.js, Express.js, SQL</p>
+            <p className='title-tech'>Technologies used: HTML/CSS, JavaScript</p>
             <p className='title-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
